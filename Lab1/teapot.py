5882136bd21de551b9e4e013462999661d409bea
Sharun <sharunpublic@gmail.com/bin/python3>
2021-08-29 12:47:31 +0530

    Add plotting a point program

--- Lab1/teapot.py
+++ Lab1/teapot.py
@@ -1,18 +1,20 @@
 # Tea pot using OpneGL Shader Language and Glut
 
-from OpenGL.GLUT import * 
-from OpenGL.GL import * 
-from OpenGL.GLU import * 
- 
-def draw(): 
-       glClear(GL_COLOR_BUFFER_BIT) 
-       glutWireTeapot(0.5) 
-       glFlush() 
- 
-glutInit(sys.argv) 
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB) 
-glutInitWindowSize(500, 500) 
-glutInitWindowPosition(200, 100) 
-glutCreateWindow("My Second OGL Program") 
-glutDisplayFunc(draw) 
+from OpenGL.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glutWireTeapot(0.5)
+    glFlush()
+
+
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+glutInitWindowSize(500, 500)
+glutInitWindowPosition(200, 100)
+glutCreateWindow("My Second OGL Program")
+glutDisplayFunc(draw)
 glutMainLoop()
