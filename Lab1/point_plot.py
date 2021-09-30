a4326a4bd1435b7aa763c84b29ba6a32a0ad8154
D_p <56536197+devaprasadnm@users.noreply.github.com>
2021-10-01 00:41:01 +0530

    Update point_plot.py

--- Lab1/point_plot.py
+++ Lab1/point_plot.py
@@ -1,35 +1,27 @@
 from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
+from OpenGL.GLU import *     
+from OpenGL.GLUT import *    
 import sys
-
-
-def init():
-    glClearColor(0.0, 0.0, 0.0, 1.0)
-    gluOrtho2D(-1.0, 1.0, -1.0, 1.0)
-
-
-def plotpoints():
+import math
+print("Packages imported successfully")
+def clearscreen(): #clears the windows
+    glClearColor(0.0, 0.0, 0.0, 1.0) 
+    gluOrtho2D(-1.0, 1.0,-1.0,1.0)
+def plot_points():
     glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(1.0, 0.0, 0.0)
-
+    glColor3f(0.0,1.0,0.0)
+    glPointSize(5.0)
     glBegin(GL_POINTS)
-    glVertex2f(0.0, 0.0)
+    glVertex2f(0.0,0.0)
     glEnd()
-
     glFlush()
-
-
 def main():
     glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitDisplayMode(GLUT_RGB)
+    glutCreateWindow("Point")
     glutInitWindowSize(500, 500)
     glutInitWindowPosition(50, 50)
-    glutCreateWindow("Plot Points")
-    glutDisplayFunc(plotpoints)
-
-    init()
+    glutDisplayFunc(plot_points)
+    clearscreen()
     glutMainLoop()
-
-
 main()
